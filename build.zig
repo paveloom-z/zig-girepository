528b822d91347dce2796b1bfccc73ff4e61ddff1
Pavel Sobolev <paveloom@riseup.net>
2023-01-20 20:39:00 +0300

    Generate the `typelib` file.

--- build.zig
+++ build.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) !void {
+    // Add standard target options
+    const target = b.standardTargetOptions(.{});
     // Add standard release options
     const mode = b.standardReleaseOptions();
     // Add the library
@@ -13,8 +15,41 @@ pub fn build(b: *std.build.Builder) !void {
     unit_tests.setBuildMode(mode);
     unit_tests_step.dependOn(&unit_tests.step);
     unit_tests.test_evented_io = true;
+    // Define the library package
+    const lib_pkg = std.build.Pkg{
+        .name = "gir",
+        .source = .{ .path = "src/lib.zig" },
+        .dependencies = &.{},
+    };
+    // Add the generate executable
+    const generate = b.addExecutable("generate", "gir/generate.zig");
+    // For each executable
+    inline for (.{
+        .{ generate, "Generate the bindings" },
+    }) |tuple| {
+        // Unpack the tuple
+        const step = tuple[0];
+        const run_step_name = tuple[1];
+        // Make sure they can be built and installed
+        step.setTarget(target);
+        step.setBuildMode(mode);
+        step.install();
+        // Add the library package
+        step.addPackage(lib_pkg);
+        // Add a run step
+        if (step.install_step) |install_step| {
+            const run_step = b.step(step.name, run_step_name);
+            const run_cmd = step.run();
+            run_cmd.step.dependOn(&install_step.step);
+            if (b.args) |args| {
+                run_cmd.addArgs(args);
+            }
+            run_step.dependOn(&run_cmd.step);
+        }
+    }
     // Add the dependencies
     inline for (.{
+        generate,
         girepository,
         unit_tests,
     }) |step| {
